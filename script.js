14ed2b9a43416b9589806eede297c4dccb570aee
Gustavo <gustavo.zuchini@etec.sp.gov.br>
2025-12-06 20:50:44 -0300

    Criando

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-window.addEventListener("load", () => {
-    setTimeout(() => {
-        document.getElementById("loading-screen").classList.add("hidden");
-        document.getElementById("main-screen").classList.remove("hidden");
-    }, 2500);
-});
-
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll(".slot.empty").forEach(slot => {
         slot.addEventListener("click", () => {
